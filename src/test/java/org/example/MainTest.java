e818568a346704497c8ce105acb7e9321874dad6
SMetto20 <smetto20@gmail.com>
2022-04-30 14:14:49 +0300

    adds encoding test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    @Test
+    public void encodeCheker_stringInput_ArrayList(){
+        Main main = new Main();
+        List<String> expectedOutput = new ArrayList<String>();
+        expectedOutput.add("andy");
+        assertEquals(expectedOutput, main.encodeWords(" "));
+    }
 
 }

02b4dbcc0ba07e9b797b8eea6135555d6fcec288
SMetto20 <smetto20@gmail.com>
2022-05-01 18:16:15 +0300

    ft adds loop to loop through plain text

--- src/main/java/org/example/Decode.java
+++ src/main/java/org/example/Decode.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Decode {
+    public static String decrypt(String cipher, int key) {
+        String text = " ";
+        for (int i = 0; i < cipher.length(); i++) {
+            char letter = cipher.charAt(i);
+        }
+    }
 }

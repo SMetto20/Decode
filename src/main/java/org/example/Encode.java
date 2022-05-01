abc283de1e9ff3829d6d49efad8b66abf79aaf49
SMetto20 <smetto20@gmail.com>
2022-05-01 18:27:33 +0300

    adds loop for cypher text

--- src/main/java/org/example/Encode.java
+++ src/main/java/org/example/Encode.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class Encode {
-}
+    public static String encrypt(String text, int key ) {
+        String cipher = " ";
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+        }
+    }

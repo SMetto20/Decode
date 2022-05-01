789a4c80e7d68924083e98485805ae5dc6987c59
SMetto20 <smetto20@gmail.com>
2022-05-01 18:20:01 +0300

    ft-adds necesarry branching for texts that are in uppecase

--- src/main/java/org/example/Decode.java
+++ src/main/java/org/example/Decode.java
@@ -5,6 +5,14 @@ public class Decode {
         String text = " ";
         for (int i = 0; i < cipher.length(); i++) {
             char letter = cipher.charAt(i);
+            if (Character.isUpperCase(letter)) {
+                int x = letter - 'A' - key;
+                if (x < 0) {
+                    x += 26;
+                }
+
+                letter = (char) (x + 'A');
         }
     }
+
 }
